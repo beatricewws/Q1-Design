cad7f86852d890159d91144a6d74ec6edb52f2c9
beatricewws <110101509+beatricewws@users.noreply.github.com>
2023-03-20 11:57:53 +1000

    Update App.js
    move const to separate file

--- src/App.js
+++ src/App.js
@@ -1,29 +1,10 @@
 import React from 'react';
+import Sidebar from './components/Sidebar';
+import MainContent from './pages/MainContent';
 import './App.css';
 
 const Header = () => {
   return <div className="header">Header</div>;
-};
-
-const Navigation = () => {
-  return (
-    <div className="navigation">
-      <ul>
-        <li>Link 1</li>
-        <li>Link 2</li>
-        <li>Link 3</li>
-      </ul>
-    </div>
-  );
-};
-
-const Content = () => {
-  return (
-    <div className="content">
-      <div className="left">Left Content</div>
-      <div className="right">Right Content</div>
-    </div>
-  );
 };
 
 const App = () => {
@@ -31,12 +12,11 @@ const App = () => {
     <div className="container">
       <Header />
       <div className="main">
-        <Navigation />
-        <Content />
+        <Sidebar />
+        <MainContent />
       </div>
     </div>
   );
 };
 
 export default App;
-
