0a272bd714fc82bdfb34637475a5232729d8aac2
beatricewws <110101509+beatricewws@users.noreply.github.com>
2023-03-20 10:55:05 +1000

    Create MainContent.js

---
+++ src/pages/MainContent.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const MainContent = () => {
+    return (
+      <main>
+        <h1>Welcome to My Website</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+      </main>
+    );
+  };
+
+  export default MainContent;
