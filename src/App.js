75b04e46aea01131744b0f53f2f05910b230794b
beatricewws <110101509+beatricewws@users.noreply.github.com>
2023-03-20 11:38:44 +1000

    Update App.js for navigation

--- src/App.js
+++ src/App.js
@@ -1,20 +1,42 @@
 import React from 'react';
-import Sidebar from './components/Sidebar';
-import MainContent from './pages/MainContent';
 import './App.css';
 
-function NavBar() {
+const Header = () => {
+  return <div className="header">Header</div>;
+};
+
+const Navigation = () => {
+  return (
+    <div className="navigation">
+      <ul>
+        <li>Link 1</li>
+        <li>Link 2</li>
+        <li>Link 3</li>
+      </ul>
+    </div>
+  );
+};
+
+const Content = () => {
+  return (
+    <div className="content">
+      <div className="left">Left Content</div>
+      <div className="right">Right Content</div>
+    </div>
+  );
+};
+
+const App = () => {
   return (
     <div className="container">
-      <div class="box">
-        <Sidebar />
-      </div>
-      <div class="box">
-        <MainContent />
+      <Header />
+      <div className="main">
+        <Navigation />
+        <Content />
       </div>
     </div>
   );
-}
+};
 
-export default NavBar;
+export default App;
 
