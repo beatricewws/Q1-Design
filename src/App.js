dd6acca4030a179fc7a38536ce8c89a7dea95b98
beatricewws <110101509+beatricewws@users.noreply.github.com>
2023-03-27 16:01:01 +1000

    Add button at header

--- src/App.js
+++ src/App.js
@@ -1,13 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Sidebar from './components/Sidebar';
 import MainContent from './pages/MainContent';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import buttonImage1 from './images/button1.png';
+import buttonImage2 from './images/button2.png';
 
 const Header = () => {
+  const [showList, setShowList] = useState(false);
+
+  const handleClick1 = () => {
+    setShowList(!showList);
+  };
+
+  const [buttonImage, setButtonImage] = useState(buttonImage1);
+
+  const handleClick2 = () => {
+    if (buttonImage === buttonImage1) {
+      setButtonImage(buttonImage2);
+    } else {
+      setButtonImage(buttonImage1);
+    }
+  };
+
+  const handleButtonClick = () => {
+    handleClick1();
+    handleClick2();
+  };
+
   return (
     <div className="header">
       <img src="./logo.png" alt="MCA logo" />
+      <div className="header1">
+        <img src="./shopping.png" alt="cart" />
+        <button style={{ outline: "none" }} onClick={handleButtonClick}>
+          <img src={buttonImage} alt="Button" />
+        </button>
+        {showList && (
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Contact</li>
+        </ul>
+        )}
+      </div>
     </div>
   );
 };
