e48d12496144d82af123918211953bae3bdc26ae
beatricewws <110101509+beatricewws@users.noreply.github.com>
2023-03-31 15:54:44 +1000

    Add TextBox

--- src/pages/MainContent.js
+++ src/pages/MainContent.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import { Login, Register } from '../components/Login';
+import TextBox from '../components/Textbox.js';
 
 const MainContent = () => {
     return (
+      <div className="Text"><TextBox />
         <div className="content">
           <div className="left"><Login /></div>
           <div className="right"><Register /></div>
         </div>
+      </div>
     );
   };
 
   export default MainContent;
+
