ee836aebe552525bb6e33d5d5b7d944dc18e2269
beatricewws <110101509+beatricewws@users.noreply.github.com>
2023-03-31 14:17:22 +1000

    update list items

--- src/components/Sidebars.js
+++ src/components/Sidebars.js
@@ -3,10 +3,15 @@ import React from 'react';
 const Sidebar = () => {
     return (
       <div className="navigation">
-        <ul>
-          <li><a href="#">Link 1</a></li>
-          <li><a href="#">Link 2</a></li>
-          <li><a href="#">Link 3</a></li>
+        <ul style={{ listStyleType: 'none' }}>
+          <li><a href="#">My Dashboard</a></li>
+          <li><a href="#">Member offers</a></li>
+          <li><a href="#">Order history</a></li>
+          <li><a href="#">Account details</a></li>
+          <li><a href="#">Address book</a></li>
+          <li><a href="#">Subscriptions</a></li>
+          <li><a href="#">Memberships history</a></li>
+          <li><a href="#">Verify Entry Numbers</a></li>
         </ul>
       </div>
     );
