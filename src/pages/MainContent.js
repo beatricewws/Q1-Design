60c362d540604f4640225605b26de2344bbff039
beatricewws <110101509+beatricewws@users.noreply.github.com>
2023-04-17 11:42:46 +1000

    Updates style container

--- src/pages/MainContent.js
+++ src/pages/MainContent.js
@@ -1,17 +1,25 @@
 import React from 'react';
 import { Login, Register } from '../components/Login';
 import TextBox from '../components/Textbox.js';
+import styled from 'styled-components';
+
+const Container = styled.div`
+  margin-left: 200px; /* replace with width of sidebar */
+`;
 
 const MainContent = () => {
     return (
+      <Container>
       <div className="Text"><TextBox />
         <div className="content">
           <div className="left"><Login /></div>
           <div className="right"><Register /></div>
         </div>
       </div>
+      </Container>
     );
   };
 
   export default MainContent;
+
 
--- src/pages/MyDashboard.js
+++ src/pages/MyDashboard.js
@@ -1,11 +1,23 @@
 import React from 'react';
+import TextBox from '../components/Textbox.js';
+import MyCar from '../components/MyCar.js';
+import styled from 'styled-components';
 import 'bootstrap/dist/css/bootstrap.min.css';
+
+const Container = styled.div`
+  margin-left: 200px; /* replace with width of sidebar */
+`;
 
 const MyDashboard = () => {
   return (
-    <div className="container">
-          <h1>My dashboard</h1>
+    <Container>
+    <div className="Text"><TextBox />
+      <div className="container">
+        <h1>My dashboard</h1>
+        <MyCar />
+      </div>
     </div>
+    </Container>
   );
 };
 
