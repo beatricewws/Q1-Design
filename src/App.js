b45fd5cd971c16277670e805f80e1a4ca514577c
beatricewws <110101509+beatricewws@users.noreply.github.com>
2023-04-17 10:25:24 +1000

    update navigate and route

--- src/App.js
+++ src/App.js
@@ -1,17 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Sidebar from './components/Sidebar';
+import Navbar from './components/Navbar';
 import Header from './components/Header';
 import MainContent from './pages/MainContent';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import MyDashboard from './pages/MyDashboard';
+
 
 const App = () => {
+  const [hideDiv, setHideDiv] = useState(false);
+
+  const handleRouteChange = () => {
+    setHideDiv(true);
+  };
+
   return (
     <div className="container">
       <Header />
       <div className="main">
-        <Sidebar />
-        <div className="login">
+        <Router>
+        <Navbar handleRouteChange={handleRouteChange} />
+        <Routes>
+            <Route path='/MyDashboard' element={<MyDashboard/>} />
+        </Routes>  
+        </Router>
+        <div className='login'  style={{ display: hideDiv ? 'none' : 'block' }}>
           <MainContent />
         </div>
       </div>
