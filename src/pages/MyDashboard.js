4657ecac3548ce0490f05b188bb0f2ce73132fd3
beatricewws <110101509+beatricewws@users.noreply.github.com>
2023-04-21 12:41:07 +1000

    Add membership card

--- src/pages/MyDashboard.js
+++ src/pages/MyDashboard.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState }from 'react';
 import MyCar from '../components/MyCar.js';
 import TextBox from '../components/Textbox.js';
 import styled from 'styled-components';
@@ -8,10 +8,11 @@ const Container = styled.div`
   margin-left: 200px; /* replace with width of sidebar */
 `;
 
-const RoundRectangle = ({ children, color, text }) => (
+const RoundRectangle = ({ children, color, text, onClick }) => (
   <div
     style={{
-      width: "100%",
+      width: "300px",
+      height: "70px",
       borderRadius: "10px",
       backgroundColor: color,
       padding: "20px",
@@ -22,6 +23,29 @@ const RoundRectangle = ({ children, color, text }) => (
       color: "#fff",
       fontWeight: "bold"
     }}
+    onClick={onClick}
+  >
+    <div>{text}</div>
+    {children}
+  </div>
+);
+
+const RoundRectangle2 = ({ children, color, text, onClick }) => (
+  <div
+    style={{
+      width: "300px",
+      height: "200px",
+      borderRadius: "10px",
+      backgroundColor: color,
+      padding: "20px",
+      display: "inline-block",
+      margin: "10px 0",
+      marginRight: "10px",
+      textAlign: "center",
+      color: "#fff",
+      fontWeight: "bold"
+    }}
+    onClick={onClick}
   >
     <div>{text}</div>
     {children}
@@ -29,6 +53,12 @@ const RoundRectangle = ({ children, color, text }) => (
 );
 
 const MyDashboard = () => {
+  const [showRectangle, setShowRectangle] = useState(false);
+
+  const handleCardClick = () => {
+    setShowRectangle(show => !show);
+  };
+
   return (
       <div className="container">
         <Container>
@@ -36,15 +66,28 @@ const MyDashboard = () => {
         <h1>My dashboard</h1>
         <h1>WELCOME BACK, .</h1>
           <div>
-            <RoundRectangle color="orange" text="Span Rectangle" style={{marginTop: "-50px"}}>
-              <p>This is some text inside the span rectangle.</p>
+            <RoundRectangle color="darkgrey" text="" style={{marginTop: "-50px"}} >
+              <p>MINI VIP</p>
             </RoundRectangle><br/>
-            <RoundRectangle color="lightblue" text="">
-              <p>MY CARD</p>
-            </RoundRectangle>
-            <RoundRectangle color="lightgreen" text="">
+          
+              <RoundRectangle color="#E9C656" text="" display="inline-block" onClick={handleCardClick}>
+                <p>MY CARD</p>
+              </RoundRectangle>
+            
+            <RoundRectangle color="grey" text="" display="inline-block">
               <p>REWARD POINTS: 250</p>
-            </RoundRectangle>
+            </RoundRectangle><br/>
+            {showRectangle && (
+              <RoundRectangle2
+                color="black"
+                style={{ marginTop: "10px" }}
+              >
+                <img src="logo.png" alt="Logo" />
+                <h2>MCA 235250</h2>
+                <p>EXPIRY DATE On-going</p>
+                <p>MEMBER SINCE 01/01/1970</p>
+              </RoundRectangle2>
+            )}
           </div>
         <MyCar />
         </div>
