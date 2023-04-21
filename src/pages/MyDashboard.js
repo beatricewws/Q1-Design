cee0fc39a5c548b4aba0a77d48e00bc99478fb57
beatricewws <110101509+beatricewws@users.noreply.github.com>
2023-04-21 11:23:15 +1000

    Add rectangles

--- src/pages/MyDashboard.js
+++ src/pages/MyDashboard.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import TextBox from '../components/Textbox.js';
 import MyCar from '../components/MyCar.js';
+import TextBox from '../components/Textbox.js';
 import styled from 'styled-components';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
@@ -8,16 +8,48 @@ const Container = styled.div`
   margin-left: 200px; /* replace with width of sidebar */
 `;
 
+const RoundRectangle = ({ children, color, text }) => (
+  <div
+    style={{
+      width: "100%",
+      borderRadius: "10px",
+      backgroundColor: color,
+      padding: "20px",
+      display: "inline-block",
+      margin: "10px 0",
+      marginRight: "10px",
+      textAlign: "center",
+      color: "#fff",
+      fontWeight: "bold"
+    }}
+  >
+    <div>{text}</div>
+    {children}
+  </div>
+);
+
 const MyDashboard = () => {
   return (
-    <Container>
-    <div className="Text"><TextBox />
       <div className="container">
+        <Container>
+        <div className="text"><TextBox />
         <h1>My dashboard</h1>
+        <h1>WELCOME BACK, .</h1>
+          <div>
+            <RoundRectangle color="orange" text="Span Rectangle" style={{marginTop: "-50px"}}>
+              <p>This is some text inside the span rectangle.</p>
+            </RoundRectangle><br/>
+            <RoundRectangle color="lightblue" text="">
+              <p>MY CARD</p>
+            </RoundRectangle>
+            <RoundRectangle color="lightgreen" text="">
+              <p>REWARD POINTS: 250</p>
+            </RoundRectangle>
+          </div>
         <MyCar />
-      </div>
+        </div>
+        </Container>      
     </div>
-    </Container>
   );
 };
 
